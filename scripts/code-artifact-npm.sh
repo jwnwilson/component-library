dbafe3dcd29939ed996aaa49065a521a1a444806
Noel Wilson <jwnwilson88@gmail.com>
2022-12-01 23:49:23 +0000

    Removing pypi

--- scripts/code-artifact-npm.sh
+++ scripts/code-artifact-npm.sh
@@ -9,7 +9,7 @@ AWS_ACCOUNT="675468650888"
 
 # Example: https://<my-domain>-<domain-owner-id>.d.codeartifact.<region>.amazonaws.com/pypi/<my-repo>/
 # Note the lack of the "aws:auth-token@" part
-export CODEARTIFACT_REPOSITORY_URL=`aws codeartifact get-repository-endpoint --domain ${DOMAIN} --domain-owner ${AWS_ACCOUNT} --repository ${REPO} --format pypi --query repositoryEndpoint --output text`
+export CODEARTIFACT_REPOSITORY_URL=`aws codeartifact get-repository-endpoint --domain ${DOMAIN} --domain-owner ${AWS_ACCOUNT} --repository ${REPO} --format npm --query repositoryEndpoint --output text`
 
 # This will give the token to access the repo
 export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain ${DOMAIN} --domain-owner ${AWS_ACCOUNT} --query authorizationToken --output text`
