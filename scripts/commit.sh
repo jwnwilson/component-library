081d9f66edbfc1887b5df7b4963de1c49592b5b9
Noel Wilson <jwnwilson88@gmail.com>
2022-12-01 19:51:43 +0000

    Readding github to known hosts

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,6 +5,10 @@ set -x
 
 THIS_VERSION=`node -p "require('./package.json').version"`
 
+mkdir ~/.ssh
+touch ~/.ssh/known_hosts
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 git checkout main
 
 git add .
